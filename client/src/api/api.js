8b9d77f0a4f554dbfb0da5d567dd75187f9acb6f
prathap0983 <prathapranger@gmail.com>
2026-07-31 13:17:53 +0530

    feat: enable local network hosting, dynamic api routing, and whatsapp fixes

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: `http://${window.location.hostname}:5000/api`
 });
 
 // Request interceptor to automatically attach JWT authorization token
