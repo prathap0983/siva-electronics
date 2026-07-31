a51ae4b9d9f3b6d07b3aabcc7cda85a3ceaeb6fd
prathap0983 <prathapranger@gmail.com>
2026-07-31 12:02:39 +0530

    Update CORS for Vercel domain

--- server/src/index.js
+++ server/src/index.js
@@ -30,7 +30,7 @@ app.use(helmet());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://sivaelectronics-liart.vercel.app"
+    "https://sivaelectronics-store.vercel.app"
   ],
   credentials: true
 }));
